5a0efbb0c92b409c9e25698cb4e1296852b4cbd3
xing.sun <xing.sun@nio.com>
2019-06-13 10:45:27 +0800

    修改数据库地址，改为127.0.0.1

--- src/config/adapter.production.js
+++ src/config/adapter.production.js
@@ -1,24 +1,24 @@
 // 生产环境配置
-const path = require('path');
+const path = require("path");
 exports.model = {
-  mysql: {
-    database: 'blog',
-    // host: '127.0.0.1',
-    // port: '',
-    // user: 'root',
-    // password: 'mx102005',
-    prefix: 'sx_',
-    encoding: 'utf8',
-    host: '47.97.161.236',
-    port: 3306,
-    user: 'root',
-    password: 'mx102005',
-    dateStrings: true
-  }
+    mysql: {
+        database: "blog",
+        // host: '127.0.0.1',
+        // port: '',
+        // user: 'root',
+        // password: 'mx102005',
+        prefix: "sx_",
+        encoding: "utf8",
+        host: "127.0.0.01",
+        port: 3306,
+        user: "root",
+        password: "mx102005",
+        dateStrings: true
+    }
 };
 
 exports.view = {
-  common: {
-    viewPath: path.join(think.ROOT_PATH, '/view')
-  }
+    common: {
+        viewPath: path.join(think.ROOT_PATH, "/view")
+    }
 };
